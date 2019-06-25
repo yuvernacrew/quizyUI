c4dc2249d80e91933d9d8084226fcadd4312e3f2
加藤　優奈 <yuvernacrew@gmail.com>
2019-06-26 08:45:38 +0900

    add new file

---
+++ pages/sp/answer.vue
@@ -0,0 +1,14 @@
+
+<template lang='pug'>
+	.container
+		.content-inner
+			
+			.question
+				p.question-text これはなんでしょうか？
+			.option
+				a.button.is-medium.is-fullwidth ①
+</template>
+
+<style>
+
+</style>
